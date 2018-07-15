be729b47cc0c11fa20d0499a52ae3691703bd41b
PolyMorpher01 <ayushshta@gmail.com>
2018-07-15 17:15:38 +0545

    implement basic react routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
-import TodoTable from './components/todoTable/index';
+import TodoList from './components/todoList';
+import TodoUpdate from './components/todoUpdate';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TodoTable/>
+        <Router>
+          <div>
+          <Route exact path="/" component={TodoList} />
+          <Route exact path="/todos/:id" component={TodoUpdate} />
+          </div>
+        </Router>
       </div>
     );
   }
